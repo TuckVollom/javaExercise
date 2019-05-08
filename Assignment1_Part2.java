7f4ee8c3e6986e4619863c18200fd1bfa9643f74
TuckVollom <49848088+TuckVollom@users.noreply.github.com>
2019-05-07 18:25:03 -0700

    Update Assignment1_Part2.java

--- Assignment1_Part2.java
+++ Assignment1_Part2.java
@@ -1,8 +1,23 @@
 //Tuck Vollom
-//CIS 35a
-//Lab 1 - Part 2
-//Due Date 		 4/21/2019
-//Date Submitted 4/21/2019
+/*
+ * Chapter 6 Exercise 22:
+ *
+ *      (Math: approximate the square root) There are several techniques for implementing
+ *      the sqrt method in the Math class. One such technique is known as the Babylonian method.
+ *      It approximates the square root of a number, n, by repeatedly performing a calculation
+ *      using the following formula:
+ *
+ *          nextGuess = (lastGuess + n / lastGuess) / 2
+ *
+ *      When nextGuess and lastGuess are almost identical, nextGuess is the approximated square root.
+ *      The initial guess can be any positive value (e.g., 1). This value will be the starting value
+ *      for lastGuess. If the difference between nextGuess and lastGuess is less than a very small
+ *      number, such as 0.0001, you can claim that nextGuess is the approximated square root of n.
+ *      If not, next- Guess becomes lastGuess and the approximation process continues. Implement
+ *      the following method that returns the square root of n.
+ *
+ *      public static double sqrt(long n)
+*/
 
 import java.util.Scanner;
 
